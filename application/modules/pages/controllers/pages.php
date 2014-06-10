25a085a2219dd6cdf2a953e2abd02c6ff0a00c0f
Nam Nguyen <nam@propagate.com.au>
2014-06-10 13:05:14 +1000

    add product landing page

--- application/modules/pages/controllers/pages.php
+++ application/modules/pages/controllers/pages.php
@@ -15,106 +15,19 @@ class Pages extends MX_Controller {
 
 	function index($method='', $param='')
 	{
-		switch($method)
+		switch($param)
 		{
-			/*case 'contact-us':
-				$this->contact_us();
-			break;
-			case'send_contact_msg':
-				$this->send_contact_msg();
-			break;
-			case 'page':
-				$this->page($param);
-			break;*/
+			case 'Resources':
+				$this->resources_landing_page();
+				break;
+			case 'Products':
+				$this->products_landing_page();
 			default:
 				$this->page($param);
 			break;
 		}
 	}
 
-	/**
-	*	@desc Shows the page content
-	*
-	*	@name page
-	*	@access public
-	*	@return shows view file
-	*	@author Propagate Dev Team - KG
-	*	@version 1.0
-	*
-	*/
-
-
-	function contact_us()
-	{
-		$this->load->view('contact', isset($data) ? $data : NULL);
-	}
-
-	function send_contact_msg()
-	{
-		$name = $this->input->post('name',true);
-		$company = $this->input->post('company',true);
-		$email = $this->input->post('email',true);
-		$phone = $this->input->post('phone',true);
-		$department = $this->input->post('department',true);
-		$message = $this->input->post('message',true);
-		$spambot = $this->input->post('spambot',true);
-		if(!$spambot){
-			$valid = true;
-			if(!$name || !$email || !$message){
-				$valid = false;
-			}
-
-			if($email){
-				if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-					$valid = false;
-				}
-			}
-
-			if($valid){
-				switch($department){
-					case 'tech-support':
-						$to = tech_support;
-					break;
-
-					case 'sales':
-						$to = sales_email;
-					break;
-
-					default:
-						$to = sales_email;
-					break;
-				}
-
-				//$to = 'raquel@propagate.com.au';
-				$email_message = '<p>Website Contact Form</p>
-
-								 <p>
-								 Name: '.$name.'<br />
-								 Email: '.$email.'<br />
-								 Phone: '.$phone.'<br />
-								 Message: '.$message.'<br />
-								 </p>
-
-							     <p>This was sent throught Contact Form @ Passing Lane web site</p>';
-
-				$email_data = array(
-								'to' => $to,
-								'from_text' => 'Passing Lane Contact Page',
-								'from' => 'noreply@passinglane.com.au',
-								'subject' => 'New Message From Contact Page at Passing Lane Website',
-								'message' => $email_message
-								);
-
-
-				$this->page_model->send_email($email_data);
-				$this->session->set_flashdata('sent',true);
-			}else{
-				$this->session->set_flashdata('error',true);
-			}
-
-		}
-		redirect('contact-us');
-	}
 
 	function company()
 	{
@@ -125,6 +38,7 @@ class Pages extends MX_Controller {
 	{
 		$page = $this->page_model->get_page_by_link($id_title);
 		$page->content = str_replace('[contact-form]', $this->contact_form(), $page->content);
+
 		$data['page'] = $page;
 		if($page->right_bar == 1)
 		{
@@ -142,6 +56,18 @@ class Pages extends MX_Controller {
 		return $this->load->view('contact_form', isset($data) ? $data : NULL, true);
 	}
 
+	function resources_landing_page()
+	{
+		$this->load->model('adminproduct/product_model');
+		$data['products'] = $this->product_model->get_similar_products('written');
+		$this->load->view('product_landing_page', isset($data) ? $data : NULL);
+	}
 
+	function products_landing_page()
+	{
+		$this->load->model('adminproduct/product_model');
+		$data['products'] = $this->product_model->get_similar_products('video');
+		$this->load->view('product_landing_page', isset($data) ? $data : NULL);
+	}
 
 }
---
+++ application/modules/pages/views/product_landing_page.php
@@ -0,0 +1,43 @@
+<div class="container">
+    <div class="content-wrap">
+        <div class="inner-content">
+            <div class="col-md-12 page-breaker">
+                <h3 class="stitched grey-bg">Resources</h3><br />
+                <ul>
+                <? foreach($products as $product) { ?>
+                 <li class="col-md-3" style="margin-bottom: 10px;">
+                  <div class="carousel-product-box">
+                      <a href="<?=base_url();?>products/<?=$product->id_title;?>">
+                      <div class="product-img-box">
+                          <?=modules::run('product/load_product_hero_thumb_image',$product->id);?>
+                      </div>
+                      <span class="product-title"><?=(strlen($product->title .' '. $product->subtitle) > 26 ? substr($product->title .' '.$product->subtitle,0,26).'..' : $product->title .' '.$product->subtitle);?></span>
+                      </a>
+                      <span class="product-price">$<?=($product->sale_price > 0 ? $product->sale_price : $product->price);?>
+                        <? if($product->price > $product->sale_price
+                            && $product->sale_price > 0) { ?>
+                          <span class="ori_price">
+                              $<?=$product->price;?>
+                          </span>
+                          <? } ?>
+                      </span>
+                      <button class="btn btn-primary add-to-cart" data-product-id="<?=$product->id?>"><i class="fa fa-plus"></i> ADD</button>
+                  </div>
+                </li>
+                <? } ?>
+                </ul><br />
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+$j(function(){
+    $j('.add-to-cart').on('click',function(){
+        var product_id = $j(this).attr('data-product-id');
+        var quantity = 1;
+        cart.add(product_id,quantity);
+    });
+
+});//ready
+
+</script>
